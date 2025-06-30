ae5cc64750a33aae4715a7a02bba4e3d704af82c
Kim Nahyeon <nhyeonii@naver.com>
2025-06-30 16:56:49 +0900

    [FEAT] #1 디자인 시스템 컬러 세팅

--- app/src/main/java/com/solux/dorandoran/core_ui/theme/Color.kt
+++ app/src/main/java/com/solux/dorandoran/core_ui/theme/Color.kt
@@ -9,3 +9,19 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Background01 = Color(0xFFFFFFFF)
+val Background02 = Color(0xFFFBFCF6)
+val Background03 = Color(0xFFDAEFDC)
+
+val Button01 = Color(0xFFB3B3B3)
+val Button02 = Color(0xFF8FDCA1)
+
+val Neutral100 = Color(0xFFFFFFFF)
+val Neutral90 = Color(0xFFE6E6E6)
+val Neutral80 = Color(0xFFCCCCCC)
+val Neutral70 = Color(0xFFB3B3B3)
+val Neutral60 = Color(0xFF1A1A1A)
+
+
+
