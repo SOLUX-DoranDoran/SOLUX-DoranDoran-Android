d15383fe05048930bf231ead4eca380090405d78
Hiimynameiss <sh2410152@sookmyung.ac.kr>
2025-07-27 20:28:34 +0900

    [MOD] #10 수정 사항 반영 (ReviewItem.kt의 ROW 제외)

--- app/src/main/java/com/solux/dorandoran/presentation/mypage/screen/EmotionShare.kt
+++ app/src/main/java/com/solux/dorandoran/presentation/mypage/screen/EmotionShare.kt
@@ -16,8 +16,6 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
@@ -25,7 +23,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.style.TextAlign
@@ -33,6 +30,7 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.solux.dorandoran.R
 import com.solux.dorandoran.core_ui.component.EmotionShareListItem
+import com.solux.dorandoran.core_ui.theme.Background01
 import com.solux.dorandoran.core_ui.theme.Background02
 import com.solux.dorandoran.core_ui.theme.Button02
 import com.solux.dorandoran.core_ui.theme.Neutral60
@@ -64,45 +62,15 @@ fun EmotionShare(
         Column(
             modifier = Modifier.fillMaxSize()
         ) {
-            Spacer(modifier = Modifier.height(20.dp))
-
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 16.dp, vertical = 12.dp),
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-
-                Icon(
-                    imageVector = ImageVector.vectorResource(id = R.drawable.ic_back),
-                    contentDescription = "뒤로 가기",
-                    modifier = Modifier
-                        .size(24.dp)
-                        .clickable { navigator.navController.popBackStack() }
-                )
-
-                Text(
-                    text = "감성 공유",
-                    style = baseBold,
-                    color = Neutral60,
-                    textAlign = TextAlign.Center
-                )
-
-                Icon(
-                    imageVector = ImageVector.vectorResource(id = R.drawable.ic_search),
-                    contentDescription = "검색",
-                    modifier = Modifier
-                        .size(24.dp)
-                )
-            }
+            EmotionShareHeader(
+                onBackClick = { navigator.navController.popBackStack() }
+            )
 
             LazyColumn(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(top = 8.dp),
+                modifier = Modifier.fillMaxSize(),
                 verticalArrangement = Arrangement.spacedBy(4.dp),
-                contentPadding = PaddingValues(bottom = 100.dp)
+                contentPadding = PaddingValues(bottom = 100.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 itemsIndexed(viewModel.emotionShareList) { index, emotion ->
                     EmotionShareListItem(
@@ -128,13 +96,54 @@ fun EmotionShare(
                 ),
             shape = CircleShape,
             containerColor = Button02,
-            contentColor = Color.White
+            contentColor = Background01
         ) {
             Icon(
-                imageVector = Icons.Default.Add,
-                contentDescription = "감성 공유 글 추가",
+                imageVector = ImageVector.vectorResource(id = R.drawable.ic_emotionsharescreen_plus),
+                contentDescription = null,
                 modifier = Modifier.size(25.dp)
             )
         }
+    }
+}
+
+@Composable
+private fun EmotionShareHeader(
+    onBackClick: () -> Unit
+) {
+    Column {
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 12.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                imageVector = ImageVector.vectorResource(id = R.drawable.ic_back),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(24.dp)
+                    .clickable { onBackClick() }
+            )
+
+            Text(
+                text = "감성 공유",
+                style = baseBold,
+                color = Neutral60,
+                textAlign = TextAlign.Center
+            )
+
+            Icon(
+                imageVector = ImageVector.vectorResource(id = R.drawable.ic_search),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(24.dp)
+            )
+        }
+
+        Spacer(modifier = Modifier.height(8.dp))
     }
 }
